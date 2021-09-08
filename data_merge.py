ab209b5ed4a14b4c1bf277ebd269da8fc062f9b6
Sam Herring <samherring111@gmail.com>
2021-09-08 12:55:22 +1000

    feat: :sparkles: Completion of Data_Merge.py

--- data_merge.py
+++ data_merge.py
@@ -1,19 +1,35 @@
 '''
 Data Integration Demo
 
-In this demo, you are required to implement a Python Script file with the name 'data_merge.py' to correctly merge records from different given data sources into one CSV output file named 'reprotingCentre_service_locations.csv".
+In this demo, you are required to implement a Python Script file with the name 'data_merge.py' to correctly merge records from different given data sources into one CSV output file named 'reportingCentre_service_locations.csv".
 
 The Python PETL Framework is recommended for this implementation.
 
 The output file "reportingCentre_service_locations.csv" consists of the following fields: 
 
-    • CentreServiceID – a unique field identifying each record from centreServices.csv 
-    • Service – the service name from centreServices.csv 
-    • CentreID – the ID for each reporting centre 
-    • Suburb – the “Suburb” from reportingCentre.csv 
-    • Postcode – the “Postcode” from reportingCentre.csv 
-    • Latitude – the “Lat” value from centreLocations.xml 
-    • Longitude – the “Lon” value from centreLocations.xml
+    ✅ CentreServiceID – a unique field identifying each record from centreServices.csv 
+    ✅ Service – the service name from centreServices.csv 
+    ✅ CentreID – the ID for each reporting centre 
+    ✅ Suburb – the “Suburb” from reportingCentre.csv 
+    ✅ Postcode – the “Postcode” from reportingCentre.csv 
+    ✅ Latitude – the “Lat” value from centreLocations.xml 
+    ✅ Longitude – the “Lon” value from centreLocations.xml
 '''
 
-import petl at etl
+import petl as etl
+
+from petl.transform.reductions import merge
+
+centreLocations = etl.fromxml('centreLocations.xml', "Centre", {
+    'CentreID': "CentreID",
+    'Lat':'Lat',
+    'Lon':'Lon'
+})
+
+centreServices = etl.fromcsv('centreServices.csv')
+reportingCentres = etl.fromcsv('reportingCentres.csv')
+
+merge1 = etl.join(centreLocations, centreServices, key="CentreID")
+merge2 = etl.join(reportingCentres, merge1, key="CentreID")
+
+etl.tocsv(merge2, 'reportingCentre_service_locations.csv')
