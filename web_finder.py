65bc2b0d8562ba8575f07973775800e6c5eda03f
Sam Herring <samherring111@gmail.com>
2021-09-08 20:01:42 +1000

    docs: :memo: Updated web_finder to reflect additional criteria from the rubrik

--- web_finder.py
+++ web_finder.py
@@ -2,11 +2,15 @@
 RESTFUL Web Service Demo
 
 In this  demo,  you  are  required  to  implement  a  Python script with the name ‘web_finder.py’ to provide two RESTful Web services:
-    • Get  reporting  centre  services by postcode. The server should accept a ‘getServices’ query from the client browser similar to “/getServices?postcode=xxxx”. After processing this request, the service should return a HTML document containing the list of available services for a given postcode, including the following fields (attributes): 
-        o Service, Suburb. 
-    • Get reporting centre by service name. The server should accept a ‘getRCentre’ query from  the client browser similar to “/getRCentre?service=xxxx”. After processing this request, the service should return a HTML document containing the list of available reporting centres for a given service, including the following fields (attributes): 
-        o CentreID, Suburb, Latitude, Longitude. 
+    ✅ Get  reporting  centre  services by postcode. The server should accept a ‘getServices’ query from the client browser similar to “/getServices?postcode=xxxx”. After processing this request, the service should return a HTML document containing the list of available services for a given postcode, including the following fields (attributes): 
+        ✅ Service, Suburb. 
+    ✅ Get reporting centre by service name. The server should accept a ‘getRCentre’ query from  the client browser similar to “/getRCentre?service=xxxx”. After processing this request, the service should return a HTML document containing the list of available reporting centres for a given service, including the following fields (attributes): 
+        ✅ CentreID, Suburb, Latitude, Longitude. 
     The Python Bottle and petl frameworks are required for this implementation. Data for the  service is from the output file “reportingCentre_service_locations.csv” of the integration demo.
+
+Other features:
+    ❌ Input Error Handling
+    ❌ Clearly Documented
 """
 
 from bottle import route, request, template, run
