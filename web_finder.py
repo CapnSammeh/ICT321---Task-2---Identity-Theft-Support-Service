c2cbcc7b42a73234c498f3cd2e0ad65a6d53b0cd
Sam Herring <samherring111@gmail.com>
2021-09-08 16:12:58 +1000

    feat: :sparkles: Modified Services Route to return valid JSON

--- web_finder.py
+++ web_finder.py
@@ -1,4 +1,4 @@
-'''
+"""
 RESTFUL Web Service Demo
 
 In this  demo,  you  are  required  to  implement  a  Python script with the name ‘web_finder.py’ to provide two RESTful Web services:
@@ -8,27 +8,42 @@ In this  demo,  you  are  required  to  implement  a  Python script with the nam
     request, the service should return a HTML document containing the list of available reporting centres for a given service, including the following fields (attributes): 
         o CentreID, Suburb, Latitude, Longitude. 
     The Python Bottle and petl frameworks are required for this implementation. Data for the  service is from the output file “reportingCentre_service_locations.csv” of the integration demo.
-'''
+"""
 
-from bottle import route, request, response, template, run
+from bottle import route, request, template, run
 import petl as etl
 
 # Get Import Data
-data = etl.fromcsv('reportingCentre_service_locations.csv')
+data = etl.fromcsv("reportingCentre_service_locations.csv")
+
 
 # Get Services Route
-@route('/getServices')
+@route("/getServices")
 def get_services():
+    # Define the Postcode by the Request Query param 'postcode'
     postcode = request.query.postcode
-    lookup = etl.lookup(data, 'Postcode', ('Service, Suburb'))
+    
+    # Lookup the Postcode from our File
+    lookup = etl.lookup(data, "Postcode", ("Suburb", "Service"))
     lookupResponse = lookup[postcode]
-    return template('{{Service}}, {{Suburb}}', Service=lookupResponse[2], Suburb=postcode)
-run(host='localhost', port=8080)
+    
+    #Iterate through the response to create our Services List, as well as identify the Suburb Name
+    servicesList = []
+    for key,value in lookupResponse:
+        servicesList.append(value)
+        suburb = key
+
+    #Finally, return a dictionary (That Bottle automatically turns into a JSON object) with our required content.
+    return dict(data={"suburb": suburb, "services":servicesList})
+
 
 # Get Reporting Centre Route
-@route('/‘getRCentre’')
-def get_services():
-    postcode = request.query.postcode
-    return template('{{CentreID}}, {{Suburb}}, {{Latitude}}, {{Longitude}}', service=postcode, suburb=postcode)
+@route("/getRCentre")
+def get_reporting_centre():
+    service = request.query.service
+    lookup = etl.lookup(data, "Service", ("CentreID", "Suburb", "Lat", "Lon"))
+    lookupResponse = lookup[service]
+    return template("{{response}}", response=lookupResponse)
 
-run(host='localhost', port=8080)
+
+run(host="localhost", port=8080, reloader=True)
