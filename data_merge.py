0486d72f69587299beca428a80778430a1943c45
Sam Herring <samherring111@gmail.com>
2021-10-15 12:28:09 +1000

    docs: :memo: Updated the inline doco for data_merge.py

--- data_merge.py
+++ data_merge.py
@@ -16,24 +16,29 @@ The output file "reportingCentre_service_locations.csv" consists of the followin
     ✅ Longitude – the “Lon” value from centreLocations.xml
 
 Other features:
-    ❌ Input Error Handling
-    ❌ Clearly Documented
+    ✅ Clearly Documented
 '''
 
+# Import the PETL Library as the variable 'etl'
 import petl as etl
 
+# Specifically, we require the 'merge' function to perform the data merge we're attempting.
 from petl.transform.reductions import merge
 
+# Structure the Centre Locations from the base XML file
 centreLocations = etl.fromxml('centreLocations.xml', "Centre", {
     'CentreID': "CentreID",
     'Lat':'Lat',
     'Lon':'Lon'
 })
 
+# Convert both the CentreServices.csv and ReportingCentres.csv files into ETL objects for us to manipulate.
 centreServices = etl.fromcsv('centreServices.csv')
 reportingCentres = etl.fromcsv('reportingCentres.csv')
 
+# Perform two (2) merges; one to merge the CentreLocations and Services on the CentreID key, and another to merge the ReportingCentre data on top.
 merge1 = etl.join(centreLocations, centreServices, key="CentreID")
 merge2 = etl.join(reportingCentres, merge1, key="CentreID")
 
+# Finally, export the result as a CSV in the root directory of the project
 etl.tocsv(merge2, 'reportingCentre_service_locations.csv')
